1158737f0a39b5f79e81a5900d2715dc97410732
Pink-Raccoon <schweglerasha@gmail.com>
2022-10-08 00:44:07 +0200

    lab2 complete

--- Lab_BitManipulations/bit_manipulations/app/main.c
+++ Lab_BitManipulations/bit_manipulations/app/main.c
@@ -31,20 +31,28 @@
 
 /// STUDENTS: To be programmed
 
-#define bit_mask_Bright 		0xCF
-#define bit_mask_Dark				0xC0 
-
+#define bit_mask_Dark 0xCF
+#define bit_mask_Bright 0xC0 
+#define bit_mask_t0 0x1
+#define bit_mask_t1 0x2
+#define bit_mask_t2 0x4
+#define bit_mask_t3 0x8
 /// END: To be programmed
 
 int main(void)
 {
     uint8_t led_value = 0;
+		
 
     // add variables below
     /// STUDENTS: To be programmed
-		
-
-
+				uint8_t counter_variable = 0;
+				uint8_t btn_value =0 ;
+				uint8_t push_event = 0;
+				uint8_t push_counter = 0; 
+				uint8_t edge_btn_state = 0x0;
+				uint8_t edge_pushed_value = 0x0;
+				uint8_t edge_push_event = 0;
 
 
     /// END: To be programmed
@@ -52,36 +60,78 @@ int main(void)
     while (1) {
         // ---------- Task 3.1 ----------
         led_value = read_byte(ADDR_DIP_SWITCH_7_0);
-
+				
 
         /// STUDENTS: To be programmed
-				led_value &=(bit_mask_Bright);
-				led_value |=(bit_mask_Dark);
-				write_byte(ADDR_LED_7_0, led_value);
+				led_value |=(bit_mask_Bright);
+				led_value &=(bit_mask_Dark);
+				
 
 
         /// END: To be programmed
-
+				write_byte(ADDR_LED_7_0, led_value);
         
 
         // ---------- Task 3.2 and 3.3 ----------
         /// STUDENTS: To be programmed
-				
-				uint8_t btn_value = read_byte(ADDR_BUTTONS);	
-				write_byte(ADDR_LED_15_8,btn_value & 0x0F);
-				for(uint8_t counter_variable = 0; btn_value==0x01; counter_variable++){
-				
-				write_byte(ADDR_LED_15_8,counter_variable & 0x0F);
-				}
-						
-				
-				
-							
+				btn_value = read_byte(ADDR_BUTTONS) & 0xF; 
+
+			
+			if( btn_value == 0x1) {
+				counter_variable++;
+				write_byte(ADDR_LED_15_8,counter_variable);
+
 			}
+			
+			if (btn_value == 0x1 && push_event == 0)  {
+				push_counter++;
+				push_event = 1;
+			} else if(btn_value == 0x0 && push_event ==1) {
+					push_event = 0;
+			}
+			
+			write_byte(ADDR_LED_31_24,push_counter);
+			edge_pushed_value = (btn_value) & (~edge_btn_state);
+			
+			
+			
+			
+			
+			
+			
+			if((edge_btn_state & bit_mask_t3) == bit_mask_t3) {
+					edge_push_event = read_byte(ADDR_DIP_SWITCH_7_0);
+					write_byte(ADDR_LED_23_16, edge_push_event);
+				}
+				else if((edge_btn_state & bit_mask_t2) == bit_mask_t2){
+					edge_push_event = (~edge_push_event);
+					write_byte(ADDR_LED_23_16, edge_push_event);
+				}
+				else if((edge_btn_state & bit_mask_t1) == bit_mask_t1){
+					edge_push_event = edge_push_event << 1;
+					write_byte(ADDR_LED_23_16, edge_push_event);
+				}
+				else if((edge_btn_state & bit_mask_t0) == bit_mask_t0){
+					edge_push_event = edge_push_event >> 1;
+					write_byte(ADDR_LED_23_16, edge_push_event);
+				}
+				else{
+					write_byte(ADDR_LED_23_16, edge_push_event);
+				}
+			
+			
+			
+			
+			
+			
+			edge_btn_state = read_byte(ADDR_BUTTONS) & 0xF; 
+			}
+			
 				
 
 
 
         /// END: To be programmed
+			
     }
 
