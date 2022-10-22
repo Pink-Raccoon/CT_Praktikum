31cf939267968e67d4357c4f3582ee49f2724ffc
Asha Schwegler <schweglerasha@gmail.com>
2022-10-22 17:01:56 +0200

    pDatatransf

--- Lab_data_transfer/Lab_data_transfer/table_asm_Task4/app/table.s
+++ Lab_data_transfer/Lab_data_transfer/table_asm_Task4/app/table.s
@@ -27,6 +27,8 @@ ADDR_LED_15_8               EQU     0x60000101
 ADDR_LED_23_16              EQU     0x60000102
 ADDR_LED_31_24              EQU     0x60000103
 ADDR_BUTTONS                EQU     0x60000210
+ADDR_DISPL_0_1				EQU     0x60000114
+ADDR_DISPL_2_3				EQU     0x60000115
 
 BITMASK_KEY_T0              EQU     0x01
 BITMASK_LOWER_NIBBLE        EQU     0x0F
@@ -37,7 +39,7 @@ BITMASK_LOWER_NIBBLE        EQU     0x0F
         AREA MyAsmVar, DATA, READWRITE
 ; STUDENTS: To be programmed
 		; Task 3.3
-data_table SPACE 16
+data_table SPACE 32
 
 
 
@@ -68,24 +70,58 @@ readInput
 		
 		LDR  R6,[R1]                       ; Read value Dip Switch 8-15 into R6
 		ands R6,R0,R6					   ; Bitmask of Dipswitch for 8-11
-		STR	 R6,[R4]					   ; Write value Dipt switch 8-15 into LED 8-15
-		;Task 3.3
+		
+		STR	 R6,[R4]					   ; Write value Dipt switch 8-15 into LED 8-11
+		STR	 R3,[R4]					   ; Write value Dipt switch 8-15 into LED 8-11
+		LSLS R6, R6, #1						; Leftshift
+
 		LDR	 R7,=data_table
-		STRB R5,[R7,R6]					   ;Dipswitch 0-7 in table with offset dipswitch value 8-11	
+	
+		STRB R5,[R7,R6]
+	
+		
+						   
+		STRH R5,[R7,R6]					   ; led 0-7 in table with offset
+		
+		 
 		
 		;3.4
-		LDR R0,=ADDR_DIP_SWITCH_31_24
-		LDR R1,=ADDR_LED_31_24
-		LDR R3,=BITMASK_LOWER_NIBBLE
+		LDR R0,=ADDR_DIP_SWITCH_31_24      ; Load address of Dipswitch 24-31 to R0
+		LDR R1,=ADDR_LED_31_24	           ; Load address of LED 24-31
+		LDR R3,=BITMASK_LOWER_NIBBLE       ; Load Value Bitmask lower nibble to R3
 		
-		LDR 	R2,[R0]
-		ands 	R2,R3,R2
-		STR 	R2,[R1]
+		LDR 	R2,[R0]					   ; Read value of Dipswitch 24-31 into R2 
+		ands 	R2,R3,R2				   ; Bitmask value in R2
 		
-		;3.5
-		LDRB 	R0,[R7,R2]				; Access table
-		LDR		R1,=ADDR_LED_23_16 
-		STRB    R0,[R1]   	    
+		STR		R2,[R1]                    ; Write value to LED 24-31 into R2
+		 
+		   	;3.5
+		LSLS    R2,R2,#1
+		LDRB	R0,[R7,R2]			; Value switch 31-24 into table with offset r2
+		
+		LDR	R1,=ADDR_LED_23_16      ; Load address 16-23 into R1
+		STRB    R0,[R1]                 ; write value of R0 into LED 16-23
+		LDR     R5,[R2]
+		
+		
+		  ;Task 4
+		LDR     R3,=ADDR_DISPL_0_1      
+		LDR		R4,=ADDR_DISPL_2_3
+		              
+		STRB	R0,[R3]
+		
+		STRB	R5,[R4]
+		
+	
+		
+
+
+		   
+
+     
+
+		
+
 		
 		
 		
