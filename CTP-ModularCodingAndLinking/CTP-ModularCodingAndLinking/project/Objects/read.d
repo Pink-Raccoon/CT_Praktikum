baa3de07bde1a457fc270830563da64768adf3bc
Pink-Raccoon <schweglerasha@gmail.com>
2022-12-17 00:16:19 +0100

    lab 11

---
+++ CTP-ModularCodingAndLinking/CTP-ModularCodingAndLinking/project/Objects/read.d
@@ -0,0 +1,2 @@
+./objects/read.o: lib_debug_with_src\read.c inc\read.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h
---
+++ CTP-ModularCodingAndLinking/CTP-ModularCodingAndLinking/project/Objects/write.d
@@ -0,0 +1,2 @@
+./objects/write.o: lib_debug_with_src\write.c inc\write.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h
